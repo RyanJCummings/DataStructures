c01ffc7db42557a676d159bbe27b9d5024054faa
Ryan Cummings <rcummings98@gmail.com>
2022-10-17 13:30:28 -0500

    naive solution

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,19 @@
+import java.io.File;
+import java.io.IOException;
+
 public class Main {
 
+    public static void main (String[] Args) throws IOException {
+        StackAndParse parser = new StackAndParse();
+        //LinkedList linkList = new LinkedList();
+        File file = new File("src/input.txt");
+        parser.readFile(file);
+
+        if (parser.checkPunctuation()) {
+            System.out.println(parser.checkPunctuation());
+        } else {
+            System.out.println("false");
+        }
+
+    }
 }
--- src/StackAndParse.java
+++ src/StackAndParse.java
@@ -1,49 +1,80 @@
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.*;
 import java.util.ArrayList;
-import java.util.Map;
-import java.util.Scanner;
 
 public class StackAndParse {
     //TODO Write a stack and use it to parse a text file.  Count balanceable punctuation and keep in mind comments and quotes inside of strings.
+    ArrayList<Character> stack = new ArrayList<>();
 
     public StackAndParse() {}
-    ArrayList<Character> stack;
-    Map<Character, Integer> punctuation = Map.of('{', 0, '}', 0, '[', 0, ']', 0, '\'', 0, '\"', 0, '<', 0, '>', 0, '(', 0, ')', 0);
+
+    //Character[] punctuation = new Character[]{'{',  '}',  '[',  ']',  '\'',  '\"',  '<',  '>',  '(',  ')'};
 
     private void push(Character ch) {
-        stack.add(0, ch);
+        stack.add(ch);
     }
 
-    /**
+    /**0,
      *
      * @return popped character from top of stack
      */
     public Character pop() {
-        Character ch = stack.get(0);
-        stack.remove(0);
+        Character ch = stack.get(stack.size() - 1);
+        stack.remove(stack.size() - 1);
         return ch;
     }
 
     /**
      *
      * @param file Input Text File
-     * @throws FileNotFoundException
+     * @throws IOException
      */
-    public void parseFile(File file) throws FileNotFoundException {
-        Scanner sc = new Scanner(file);
-        Character currentChar = sc.next().charAt(0);
-        for (Character i : punctuation.keySet() ) {
-            if (currentChar.equals(i)) {
-                push(currentChar);
-                punctuation.put(currentChar, punctuation.get(currentChar) + 1);
+    public void readFile(File file) throws IOException {
+        int r;
+
+        try (InputStream in = new FileInputStream(file);
+             Reader reader = new InputStreamReader(in)) {
+            while ((r = reader.read()) != -1) {
+                char ch = (char) r;
+                push(ch);
             }
         }
     }
 
+    /**
+     *
+     * @return Boolean representing whether all punctuation is balanced.
+     */
     public Boolean checkPunctuation() {
-        Character ch = pop();
+        Character ch;
+        int curly = 0;
+        int square = 0;
+        int angle = 0;
+        int paren = 0;
+        int singleQuote = 0;
+        int doubleQuote = 0;
 
+        while(!stack.isEmpty()) {
+            ch = pop();
+            if (ch.equals('{') || ch.equals('}')) {
+                curly++;
+            } else if (ch.equals('[') || ch.equals(']')) {
+                square++;
+            } else if (ch.equals('\'')) {
+                singleQuote++;
+            } else if (ch.equals('\"')) {
+                doubleQuote++;
+            } else if (ch.equals('<') || ch.equals('>')) {
+                angle++;
+            } else if (ch.equals('(') || ch.equals(')')) {
+                paren++;
+            }
+        }
+
+        if (curly % 2 == 0 && square % 2 == 0 && angle % 2 == 0
+                && paren % 2 == 0 && singleQuote % 2 == 0 &&
+                doubleQuote % 2 == 0) {
+            return true;
+        }
         return false;
     }
 }
